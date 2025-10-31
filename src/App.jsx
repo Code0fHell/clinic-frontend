64d4ea30a6cae5ac90618eb46505780ee97eda87
quangcn625 <quanglg2311@gmail.com>
2025-10-31 08:46:18 +0700

    update: index.html, package, App.jsx & index.css

--- src/App.jsx
+++ src/App.jsx
@@ -6,15 +6,32 @@ import RegisterPage from './pages/auth/RegisterPage';
 import AppRouter from './routes/AppRouter';
 import HomePage from './pages/HomePage';
 import MakeAppointmentPage from './pages/appointment/MakeAppointmentPage';
+import ReceptionistDashboard from './pages/receptionist/Dashboard';
+import AppointmentPage from './pages/receptionist/Appointment';
+import PatientPage from './pages/receptionist/Patient';
+import InvoicePage from './pages/receptionist/Invoice';
+import TaskPage from './pages/receptionist/Task';
+import PrivateRoute from './routes/PrivateRoute';
+
 function App() {
   return (
     <AuthProvider>
       <Router>
         <Routes>
-          <Route path='/home' element={<HomePage/>} />
+          <Route path='/home' element={<HomePage />} />
           <Route path="/login" element={<LoginPage />} />
           <Route path="/register" element={<RegisterPage />} />
-          <Route path="/patient/booking" element={<MakeAppointmentPage/>} />
+          <Route path="/patient/booking" element={<MakeAppointmentPage />} />
+
+          {/* Protected routes */}
+          <Route element={<PrivateRoute />}>
+            <Route path="/receptionist/home" element={<ReceptionistDashboard />} />
+            <Route path="/receptionist/appointment" element={<AppointmentPage />} />
+            <Route path="/receptionist/patient" element={<PatientPage />} />
+            <Route path="/receptionist/invoice" element={<InvoicePage />} />
+            <Route path="/receptionist/task" element={<TaskPage />} />
+          </Route>
+
           {/* Main app routes */}
           <Route path="/*" element={<AppRouter />} />
           {/* Redirect unknown routes */}
