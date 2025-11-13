7040e2141424da7ebb635f11729fa33baaedc011
quangcn625 <quanglg2311@gmail.com>
2025-11-14 03:03:23 +0700

    update src/api/visit.api.js

--- src/api/visit.api.js
+++ src/api/visit.api.js
@@ -1,5 +1,34 @@
+import axiosClient from "./axiosClient";
+
 // Tạo phiếu thăm khám
-export const createVisit = async (dto) => {
-    const res = await axiosClient.post("/visit/create", dto);
-    return res.data;
+export const createVisit = async (data) => {
+    try {
+        const res = await axiosClient.post("/visit/create", data);
+        return res.data;
+    } catch (err) {
+        console.error("Lỗi khi tạo visit:", err.response?.data || err);
+        throw err;
+    }
 };
+
+// Lấy danh sách thăm khám trong ngày
+export const getTodayVisit = async () => {
+    try {
+        const res = await axiosClient.get("/visit/queue");
+        return res.data;
+    } catch (err) {
+        console.error("Lỗi khi khi lấy Visit:", err.response?.data || err);
+        return [];
+    }
+}
+
+// Lấy chi tiết Visit để tạo bill
+export const getDetailVisit = async (visitId) => {
+    try {
+        const res = await axiosClient.get(`/visit/${visitId}`, {});
+        return res.data;
+    } catch (error) {
+        console.error('Lỗi khi lấy visit:', error.response?.data || error.message);
+        throw error;
+    }
+}
