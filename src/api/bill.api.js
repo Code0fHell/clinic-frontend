4a93a75299db10d120132ae0621c3468d1b154eb
quangcn625 <quanglg2311@gmail.com>
2025-12-07 19:58:28 +0700

    feat: add API for bill & payment

--- src/api/bill.api.js
+++ src/api/bill.api.js
@@ -11,3 +11,27 @@ export const createBill = async (payload) => {
         throw error;
     }
 }
+
+// Lấy bill theo ngày
+export const getAllBillToday = async () => {
+    try {
+        const res = await axiosClient.get(`/bill/all-bill-today`);
+        return res.data;
+    } catch (error) {
+        // Bạn có thể xử lý lỗi ở đây hoặc throw ra component gọi
+        console.error('Lỗi khi lấy tất cả bill trong ngày: ', error.response?.data || error.message);
+        throw error;
+    }
+}
+
+// Lấy chi tiết Bill
+export const getDetailBill = async (billId) => {
+    try {
+        const res = await axiosClient.get(`/bill/${billId}`);
+        return res.data;
+    } catch (error) {
+        // Bạn có thể xử lý lỗi ở đây hoặc throw ra component gọi
+        console.error('Lỗi khi chi tiết Bill: ', error.response?.data || error.message);
+        throw error;
+    }
+}
