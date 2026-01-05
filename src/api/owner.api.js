a2759449e87ae70953019c39133b3f763add7dd4
quangcn625 <quanglg2311@gmail.com>
2026-01-06 06:41:20 +0700

    feat: add api for owner & update library

--- src/api/owner.api.js
+++ src/api/owner.api.js
@@ -30,3 +30,98 @@ export const getRevenueService_Breakdown = async () => {
     }
 }
 
+//Lấy lịch làm việc của lễ tân, dược sĩ, bác sĩ
+export const getWeeklyScheduleOwner = async ({
+    start_date,
+    roleType = "all",
+    cursor = null,
+    limit = 10,
+} = {}) => {
+    try {
+        const params = {
+            start_date, // YYYY-MM-DD
+            roleType,
+            limit,
+        };
+
+        // chỉ gửi cursor khi load page tiếp theo
+        if (cursor !== null && cursor !== undefined) {
+            params.cursor = cursor;
+        }
+
+        const res = await axiosClient.get("/owner/weekly", { params });
+
+        return {
+            data: res.data.data || [],
+            nextCursor: res.data.nextCursor ?? null,
+        };
+    } catch (err) {
+        console.error(
+            "Lỗi khi lấy lịch làm việc tuần:",
+            err.response?.data || err.message
+        );
+
+        return {
+            data: [],
+            nextCursor: null,
+        };
+    }
+};
+
+// Lấy chi tiết lịch làm việc của bác sĩ trong 1 ngày
+export const getDoctorDailySchedule = async (scheduleId) => {
+    try {
+        const res = await axiosClient.get(`/owner/work-schedules/${scheduleId}`);
+
+        return {
+            data: res.data || [],
+        };
+    } catch (err) {
+        console.error(
+            "Lỗi khi lấy chi tiết lịch làm việc của bác sĩ trong 1 ngày:",
+            err.response?.data || err.message
+        );
+
+        return {
+            data: []
+        };
+    }
+};
+
+// Xuất excel bảng chi tiết doanh thu
+export const exportRevenueExcel = async ({
+    startDate,
+    endDate,
+    timeframe,
+}) => {
+    const res = await axiosClient.get('/owner/export/revenue-detail', {
+        params: {
+            start: startDate,   // YYYY-MM-DD
+            end: endDate,       // YYYY-MM-DD
+            timeframe,          // DAY | WEEK | MONTH | QUARTER
+        },
+        responseType: 'blob',
+    });
+
+    // Tạo file download
+    const blob = new Blob([res.data], {
+        type: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
+    });
+
+    const url = window.URL.createObjectURL(blob);
+    const link = document.createElement('a');
+
+    const today = new Date().toISOString().slice(0, 10);
+
+    link.href = url;
+    link.download = `doanh_thu_${timeframe}_${startDate}_den_${endDate}_${today}.xlsx`;
+
+    document.body.appendChild(link);
+    link.click();
+
+    // Cleanup
+    document.body.removeChild(link);
+    window.URL.revokeObjectURL(url);
+};
+
+
