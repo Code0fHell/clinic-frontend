cf2aa97cb15390520e5e9a1f57bf0c1ae6445454
quangcn625 <quanglg2311@gmail.com>
2026-01-05 12:55:37 +0700

    feat: add file ConfirmModal & update component of receptionist

--- src/pages/receptionist/components/AppointmentOverview.jsx
+++ src/pages/receptionist/components/AppointmentOverview.jsx
@@ -1,25 +1,57 @@
 import React from "react";
+import { useState, useEffect } from "react";
+import { getCountAppointmentToday } from "../../../api/appointment.api";
 
 export default function AppointmentOverview() {
+    const [total, setTotal] = useState(0);
+    // const [pending, setPending] = useState(0);
+    const [checkedIn, setCheckedIn] = useState(0);
+    const [doing, setDoing] = useState(0);
+    const [completed, setCompleted] = useState(0);
+    const [cancel, setCancel] = useState(0);
+
+    const fetchDataCountAppointment = async () => {
+        try {
+            const res = await getCountAppointmentToday();
+            // console.log("CountAppointment: " + JSON.stringify(res));
+            setTotal(res.total);
+            // setPending(res.pending);
+            setCheckedIn(res.check_in);
+            setDoing(res.doing);
+            setCompleted(res.completed);
+            setCancel(res.cancelled)
+
+        } catch (error) {
+            console.error(error);
+        }
+    }
+
+    useEffect(() => {
+        fetchDataCountAppointment();
+    }, [])
+
     return (
-        <div className="bg-white rounded-xl shadow-sm p-4">
-            {/* <h3 className="text-sm font-medium text-gray-600 mb-5">Lịch hẹn tổng quan</h3> */}
+        <div className="bg-white rounded-xl shadow-sm p-3">
             <h3 className="text-xl font-bold text-gray-800 mb-5">Lịch hẹn tổng quan</h3>
-            <div className="grid grid-cols-4 gap-8 text-center">
+            <div className="grid grid-cols-5 gap-8 text-center">
                 <div>
-                    <p className="text-4xl font-bold text-teal-700">46</p>
+                    <p className="text-3xl font-bold text-teal-700">{total}</p>
                     <p className="text-sm text-gray-500 mt-2">Tổng</p>
                 </div>
                 <div>
-                    <p className="text-4xl font-bold text-green-600">40</p>
-                    <p className="text-sm text-gray-500 mt-2">Đã hoàn thành</p>
+                    <p className="text-3xl font-bold text-purple-500">{checkedIn}</p>
+                    <p className="text-sm text-gray-500 mt-2">Đã đến </p>
                 </div>
                 <div>
-                    <p className="text-4xl font-bold text-yellow-600">40</p>
-                    <p className="text-sm text-gray-500 mt-2">Đang thực hiện</p>
+                    <p className="text-3xl font-bold text-yellow-600">{doing}</p>
+                    <p className="text-sm text-gray-500 mt-2">Đang khám</p>
                 </div>
                 <div>
-                    <p className="text-4xl font-bold text-red-600">6</p>
+                    <p className="text-3xl font-bold text-green-600">{completed}</p>
+                    <p className="text-sm text-gray-500 mt-2">Đã khám xong</p>
+                </div>
+                <div>
+                    <p className="text-3xl font-bold text-red-600">{cancel}</p>
                     <p className="text-sm text-gray-500 mt-2">Đã hủy</p>
                 </div>
             </div>
